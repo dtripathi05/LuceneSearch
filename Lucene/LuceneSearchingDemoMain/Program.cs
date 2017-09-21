fbb42dd00afc521d53aeca871a3c90dfbb8d2883
Deependra Tripathi <dtripathi@tavisca.com>
2017-09-21 22:01:04 +0530

    Initial Phase LuceneIndexer

---
+++ Lucene/LuceneSearchingDemoMain/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LucentDemo;
+namespace LuceneSearchingDemoMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                List<PointOfInterest> InterestsList = PointOfInternetDataRepository.GetAll();
+                LuceneSearch.AddUpdateLuceneIndex(InterestsList);
+                Console.WriteLine("\nProvide Your Interest ");
+                List<PointOfInterest> result = LuceneSearch.Search(Console.ReadLine()).ToList();
+                if (result[0] != null)
+                {
+                    Console.WriteLine($"Name : {result[0].Name}\nType : {result[0].Type}\nDescription : {result[0].Description}");
+                }
+                else Console.WriteLine("No Matching Record Found");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lucene/LucentDemo/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene;
+using System.Net.Http;
+
+namespace LucentDemo
+{
+    public class PointOfInterest
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+    public static class PointOfInternetDataRepository
+    {
+        public static PointOfInterest Get(string name)
+        {
+            return GetAll().SingleOrDefault(x => x.Name.Equals(name));
+        }
+        public static List<PointOfInterest> GetAll()
+        {
+            return new List<PointOfInterest> {
+        new PointOfInterest { Name = "VivantaTaj",Type="Hotel", Description = "5 Star Hotel Chain In India"},
+        new PointOfInterest { Name = "Kingfisher",Type="Flight", Description = "Airlines Provider"},
+        new PointOfInterest { Name = "Chicago",Type="City", Description = "City in USA"},
+        new PointOfInterest { Name = "Mumbai",Type="City", Description = "City In India Where Dreams Come True"},
+        new PointOfInterest { Name = "Hayat",Type="Hotel", Description = "One Of The Renewed Hotel Of Pune City "},
+    };
+        }
+    }
+}
+
